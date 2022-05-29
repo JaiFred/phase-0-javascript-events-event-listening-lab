f3363aa5d50dd98d8859ee0551d9d65ec04225da
JaiVon <frederickjaivon@yahoo.com>
2022-05-28 22:40:03 -0400

    commit tree 1a081aac44254c537b73beaf5615ca063902158a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+clickAlert
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+    alert('I was clicked!');
+    }
+input.addEventListener('Click Me!', clickAlert);
 }
+input.addEventListener('Click Me!', clickAlert);
